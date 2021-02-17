e631f65b93291b6ef0bb7e5b3dd1914e233a37af
Samuel Wilson <samwil56@gmail.com>
2021-02-17 23:28:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(line) {
 function currentLine(line) {
   var len = line.length -1
   return 'The line is currently: '
-  for (i=0, i<= len, i++) {
+  for (i=0, i<= len, i+=1) {
     var position = i+1
     return `${poistion}. ${line[i]}`
     if (i != len) {return ', '}

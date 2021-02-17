b58d5c11e70fdaf4b032ec4b150738be5153abfe
Samuel Wilson <samwil56@gmail.com>
2021-02-17 23:02:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line, name) {
   line.push(name)
-  var place = list.indexOf(name) +1
+  var place = line.indexOf(name) +1
   return `Welcome, ${name}. You are number ${place} in line.`
 }

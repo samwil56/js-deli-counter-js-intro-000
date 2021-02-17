1ad65a7d45e043244d735f4bc8de61e5f69e649a
Samuel Wilson <samwil56@gmail.com>
2021-02-17 23:04:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber(line, name) {
   var place = line.indexOf(name) +1
   return `Welcome, ${name}. You are number ${place} in line.`
 }
+
+function nowSwerving(line) {
+  var first = line[0]
+  if line == [] {return "There is nobody waiting to be served!"}
+  else {return `Currently serving ${first}.`}
+  line.shift(first)
+}

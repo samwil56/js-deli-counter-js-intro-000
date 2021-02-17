6e60e2db60de980c2188d0e6aba8e8c68503e2cd
Samuel Wilson <samwil56@gmail.com>
2021-02-17 23:32:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function nowServing(line) {
 
 function currentLine(line) {
   var len = line.length -1
+  if (line[0] == undefined) { return 'The line is currently empty.'}
   for (i=0; i<= len; i+=1) {
     var position = i+1
-    if (i==0) {return 'The line is currently: '}
+    if (i = =0) {return 'The line is currently: '}
     return `${poistion}. ${line[i]}`
     if (i != len) {return ', '}
   }
